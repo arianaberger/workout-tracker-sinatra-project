585565476a671990f0142b62381715c83a6e0d4c
Ariana Berger <ariana.berger@gmail.com>
2018-05-23 13:54:55 +0000

    created shell code for workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,30 @@
 class WorkoutsController < ApplicationController
 
+  get '/workouts' do
 
+  end
+
+  get '/workouts/new' do
+
+  end
+
+  post '/workouts/new' do
+
+  end
+
+  get '/workouts/:id' do
+
+  end
+
+  get '/workouts/:id/edit' do
+
+  end
+
+  patch '/workouts/:id' do
+
+  end
+
+  delete '/workouts/:id/delete' do
+    
+  end
 end
