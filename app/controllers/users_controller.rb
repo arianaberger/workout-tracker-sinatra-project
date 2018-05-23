e69fb7c9611aef9d503abea5ba95ac44e063bbc9
Ariana Berger <ariana.berger@gmail.com>
2018-05-23 13:52:07 +0000

    added shell code for user controller + logout code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,28 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+
+  end
+
+  post 'signup' do
+
+  end
+
+  get 'login' do
+
+  end
+
+  post 'login' do
+
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end
