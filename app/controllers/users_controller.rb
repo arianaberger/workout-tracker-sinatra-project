ea9f40a14bde9d4848315b98c953141e124993e4
Ariana Berger <ariana.berger@gmail.com>
2018-05-24 21:42:09 +0000

    added create_user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
 
   get '/signup' do
+    erb :'/user/create_user'
+  end
+
+  post '/signup' do
+    #get params and create a user
+    #first check if username is taken!
+    #then redirect to /workouts
+
+    #else redirect to /signup with flash message of "username taken"
+  end
+
+  get '/workouts' do
 
   end
 
-  post 'signup' do
-
-  end
-
-  get 'login' do
-
-  end
-
-  post 'login' do
-
-  end
 
   get '/logout' do
     if logged_in?
