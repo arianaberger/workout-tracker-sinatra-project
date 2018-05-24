7203f92ccf7dd06c617cab777465000bc43244f4
Ariana Berger <ariana.berger@gmail.com>
2018-05-24 14:23:41 +0000

    created workout_movements table

---
+++ db/migrate/20180524142203_create_workout_movements.rb
@@ -0,0 +1,10 @@
+class CreateWorkoutMovements < ActiveRecord::Migration[5.2]
+  def change
+    create_table :workout_movements do |t|
+      t.integer :workout_id
+      t.integer :movement_id
+      t.integer :reps
+      t.integer :weight
+    end
+  end
+end
