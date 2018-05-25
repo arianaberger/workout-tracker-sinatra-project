acfe3f229cce701e52b3f03e2bc63013a206a8b1
Ariana Berger <ariana.berger@gmail.com>
2018-05-25 00:57:47 +0000

    added new movement form

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,6 +1,14 @@
 class MovementsController < ApplicationController
 
+ get '/movements' do
+   erb :'/movements/movements'
+ end
+
   get '/movements/new' do
     erb :'movements/new'
+  end
+
+  post '/movements/new' do
+    
   end
 end
