f91b1114dc029969821fa68f66821eea7bcf00a9
Ariana Berger <ariana.berger@gmail.com>
2018-05-28 20:33:16 +0000

    continued logged_in? authentication for workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -25,13 +25,11 @@ class WorkoutsController < ApplicationController
     #creating a new workout, adds it to the user and saves user
       @workout = Workout.create(params[:workout])
       current_user.workouts << @workout
-
     #create variable to represent movement found by the name in params
     #add row to workout_movements table with all necessary info
     #need to iterate through each workout added in the form
       @movement = Movement.find_by(:name => params[:movement][:name])
       @w_m = WorkoutMovement.create(:workout_id => @workout.id, :movement_id => @movement.id, :weight => params[:weight], :reps => params[:reps])
-
       redirect to '/workouts'
     else
       redirect to '/login'
@@ -51,12 +49,11 @@ class WorkoutsController < ApplicationController
     if logged_in?
       @workout = Workout.find_by_id(params[:id])
       if @workout && @workout.user == current_user
-
-      # does @w_m need to be an array, so I can put multiple movements in it?
-      @w_m = WorkoutMovement.find_by(params[:id])
-      #how to iterate through the movements and match them up so they are properly selected in edit form?
-      @movement = Movement.find_by_id(@w_m.movement_id)
-      erb :'/workouts/edit'
+        # does @w_m need to be an array, so I can put multiple movements in it?
+        @w_m = WorkoutMovement.find_by(params[:id])
+        #how to iterate through the movements and match them up so they are properly selected in edit form?
+        @movement = Movement.find_by_id(@w_m.movement_id)
+        erb :'/workouts/edit'
       else
         redirect to '/workouts'
       end
@@ -66,12 +63,19 @@ class WorkoutsController < ApplicationController
   end
 
   patch '/workouts/:id' do
-    @workout = Workout.find_by_id(params[:id])
-    @workout.update(params[:workout])
-    @movement = Movement.find_by(:name => params[:movement][:name])
-    @w_m = WorkoutMovement.update(:workout_id => @workout.id, :movement_id => @movement.id, :weight => params[:weight], :reps => params[:reps])
-
-    redirect to "/workouts/#{@workout.id}"
+    if logged_in?
+      @workout = Workout.find_by_id(params[:id])
+      if @workout && @workout.user == current_user
+        @workout.update(params[:workout])
+        @movement = Movement.find_by(:name => params[:movement][:name])
+        @w_m = WorkoutMovement.update(:workout_id => @workout.id, :movement_id => @movement.id, :weight => params[:weight], :reps => params[:reps])
+        redirect to "/workouts/#{@workout.id}"
+      else
+        redirect to '/workouts'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   delete '/workouts/:id' do
